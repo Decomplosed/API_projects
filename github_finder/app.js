d6199c8a3c1641fbe47d9b328b50e8c12c86eb54
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-03 12:19:23 +0100

    added alert message if user not found and timeout for showing the alert popup which will disappear after 1,5s

--- github_finder/app.js
+++ github_finder/app.js
@@ -9,8 +9,8 @@ searchUser.addEventListener('keyup', (e) => {
   if(userText !== '') {
     github.getUser(userText)
     .then(data => {
-      if(data.profile.message === 'Not found') {
-        
+      if(data.profile.message === 'Not Found') {
+        ui.showAlert('User not found', 'alert alert-danger')
       } else {
         ui.showProfile(data.profile)
       }
